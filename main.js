0f8549536ddd0b1ea72acec654c5ea02a6afe99d
c.mariselvan <mari20041999@gmail.com>
2022-09-12 10:30:46 +0530

    add files

---
+++ main.js
@@ -0,0 +1,15 @@
+//function declaration
+function add(value1,value2){
+return  sum=value1+value2;//return value go to function calling 
+}
+//function calling
+console.log(add(5,4));
+console.log(add(20,10));
+//function called to button onclick time 
+function changeValue(){
+    a=10;
+    b=20;
+    console.log(a+b);
+}
+
+
